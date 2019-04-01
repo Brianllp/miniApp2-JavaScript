d871218d8e7dcfcc2b0c1259cafa23f8d5f6a964
English <tailangs97@gmail.com>
2019-04-01 23:08:28 +0900

    リプレイ時の動作修正

--- js/main.js
+++ js/main.js
@@ -5,23 +5,35 @@
   let stop = document.getElementById('stop');
   let result = document.getElementById('result');
   let startTime;
+  let isStarted = false;
 
+  //スタートボタンを押したときの処理
   start.addEventListener('click', function() {
+    if (isStarted === true) {　//returnで関数内の処理が止まる？
+      return;
+    }
+    isStarted = true;
     startTime = Date.now();
     this.className = 'pushed';
     stop.className = '';
+    result.textContent = '0.000';
+    result.className ='standby';
   });
 
+  //ストップボタンを押したときの処理
   stop.addEventListener('click', function() {
     let elapsedTime;
     let diff;
+    if (isStarted === false) {　//returnで関数内の処理が止まる？
+      return;
+    }
+    isStarted = false;
     elapsedTime = (Date.now() - startTime) / 1000;  //ミリ秒から秒への変換
-    // result.textContent = elapsedTime;
     result.textContent = elapsedTime.toFixed(3);  //小数点以下3桁まで表示する
     this.className = 'pushed';
     start.className = '';
+    result.className = '';
     diff = elapsedTime - 5.0;
-    // if (diff > -1.0 && diff < 1.0)
     if (Math.abs(diff) < 1.0) {  //diffの絶対値をとる
         result.className = 'perfect';
     }
